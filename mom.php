f0da240a6d6f56ba9623b457ec4edffdf3721b48
GEORG <pp057105@gmail.com>
2024-08-20 23:26:49 +0700

    first

---
+++ mom.php
@@ -0,0 +1,10 @@
+<head>
+<body>
+    <?php
+        for ($i = 1; $i <= 12; $i++) {
+            echo $_POST["number"]."*". ".$i."." = ".$_POST["number"]*$i."<br>";
+        }
+    ?>
+    
+</body>
+</html>
